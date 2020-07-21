e9619074a59c586d34a1acb1b3fc432f41d72ac1
jewer <emil@pernik.org>
2020-07-21 11:49:31 +0300

    feat: hook in react router to the app

--- react-frontend/src/index.tsx
+++ react-frontend/src/index.tsx
@@ -5,12 +5,15 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
     <Provider store={store}>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
+        <BrowserRouter>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root'),
 )
