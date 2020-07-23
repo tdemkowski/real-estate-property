6bfc0dcf52f88db61be18a25b5de5f59197bbbb0
Tom <tdemkowski96@gmail.com>
2020-07-24 02:06:53 +1000

    added: homepage feed structure with two sample posts

--- react-frontend/src/Pages/HomePage/homepage.component.tsx
+++ react-frontend/src/Pages/HomePage/homepage.component.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
-
+import Feed from './Feed/feed.component'
 import './homepage.styles.scss'
+
+import sampleImage_1 from './RemoveLater/carbonDating.png'
+import sampleImage_2 from './RemoveLater/chungus.jpg'
 
 const Homepage = () => {
     return (
         <div>
-            <div className="home-page">HOME PAGE</div>
+            <div className="home-page">
+                <Feed user="Person1" image={sampleImage_1}/>
+            </div>
+            <div className="home-page">
+                <Feed user="Person2" image={sampleImage_2}/>
+            </div>
         </div>
     )
 }
