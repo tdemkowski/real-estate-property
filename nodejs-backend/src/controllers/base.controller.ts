dcb66284bd70baabad9871286871a3dcc4fc4a82
Tom <tdemkowski96@gmail.com>
2020-07-27 23:19:37 +1000

    change: removed setHeader code from base.controllers/base.controller.ts

--- nodejs-backend/src/controllers/base.controller.ts
+++ nodejs-backend/src/controllers/base.controller.ts
@@ -6,11 +6,6 @@ import { IBaseService } from '../models/IBaseService.model';
 class BaseController<T extends BaseEntity, DTO> {
     protected router = Router();
 
-    protected setHeader = (res) => {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    }
-
     constructor(protected readonly path: string, protected readonly service: IBaseService<T>) {
         this.initializeRoutes();
     }
@@ -24,15 +19,11 @@ class BaseController<T extends BaseEntity, DTO> {
     }
 
     protected FetchAll = async (_req: Request, res: Response, _next: NextFunction) => {
-        this.setHeader(res)
-
         const findAll = await this.service.FindAll()
         res.send({response: findAll})
     }
     
     protected Create = async (req: Request, res: Response, next: NextFunction) => {
-        this.setHeader(res)
-
         const body = req.body as DTO;
         if (body) {
             const insert = await this.service.Create(body);
@@ -43,8 +34,6 @@ class BaseController<T extends BaseEntity, DTO> {
     }
 
     protected Update = async (req: Request, res: Response, next: NextFunction) => {
-        this.setHeader(res)
-
         const body = req.body as DTO;
         const userId = req.params.id;
         if (body) {
@@ -56,15 +45,11 @@ class BaseController<T extends BaseEntity, DTO> {
     }
 
     protected Delete = async (req: Request, res: Response, _next: NextFunction) => {
-        this.setHeader(res)
-
         await this.service.Delete(req.params.id)
         res.send({response: "Object deleted"})
     }
 
     protected GetById = async (req: Request, res: Response, next: NextFunction) => {
-        this.setHeader(res)
-
         const obj = await this.service.FindOne(req.params.id);
         if (obj) {
             res.status(200).send(obj);
