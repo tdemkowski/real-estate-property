dc82eb5ec2c860d872a27d4a524da1e727eace86
jewer <emil@pernik.org>
2020-08-19 19:26:54 +0300

    feat: add guard route

---
+++ react-frontend/src/HighOrderComponents/guard-route.hoc.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Redirect } from "react-router-dom";
+
+interface IProps {
+    auth: boolean;
+    component: (props: any) => JSX.Element;
+    path: string;
+}
+
+const GuardedRoute = ({auth, component: WrappedComponent, ...rest} : IProps) => (
+    <Route {...rest} render={(props) => (
+        auth === true
+            ? <WrappedComponent {...props} />
+            : <Redirect to='/' />
+        )} />
+
+)
+
+   
+
+export default GuardedRoute;
