c930339a03b5adc03361d4e9340c4c706adab89f
Tom <tdemkowski96@gmail.com>
2020-09-08 20:25:36 +1000

    fixed: homepage spamming feed request

--- nodejs-backend/src/controllers/user.controller.ts
+++ nodejs-backend/src/controllers/user.controller.ts
@@ -3,7 +3,6 @@ import UserService from '../services/user.service'
 import BaseController from './base.controller'
 import { User } from '../entities/user.entity'
 import { Router } from 'express'
-import NotFoundException from './exceptions/NotFound.exception'
 import OKSuccess from './success/OK.success'
 import BadRequest from './exceptions/BadRequest.exception'
 
@@ -25,7 +24,6 @@ class UserController extends BaseController<User, UserDTO> {
     public FetchAll = async (req: Request, res: Response, next: NextFunction) => {
         const { take } = req.query
         const findAll = await this.service.FindAll({ take: Number(take) })
-        console.log(findAll)
         if (findAll) {
             next(new OKSuccess(res, { response: findAll }))
         } else {
@@ -33,18 +31,17 @@ class UserController extends BaseController<User, UserDTO> {
         }
     }
 
-    public GetById = async (req: Request, res: Response, next: NextFunction) => {
-        const id = req.params.id
-        console.log(id)
-        const user = await this.service.FindOne(id, {
-            relations: ['posts'],
-        })
-        if (user) {
-            res.send(user)
-        } else {
-            next(new NotFoundException(id))
-        }
-    }
+    // public GetById = async (req: Request, res: Response, next: NextFunction) => {
+    //     const id = req.params.id
+    //     const user = await this.service.FindOne(id, {
+    //         relations: ['posts'],
+    //     })
+    //     if (user) {
+    //         res.send(user)
+    //     } else {
+    //         next(new NotFoundException(id))
+    //     }
+    // }
 }
 export interface UserDTO {
     username: string
--- react-frontend/src/Pages/HomePage/homepage.component.tsx
+++ react-frontend/src/Pages/HomePage/homepage.component.tsx
@@ -7,12 +7,12 @@ import './homepage.styles.scss'
 import Suggestion from '../../Components/Suggestions/suggestion.component'
 import User from '../../Components/Suggestions/User/user.component'
 import InfiniteScroll from 'react-infinite-scroller'
-import PropTypes from 'prop-types'
 
 import apiUrl from '../../config'
 import { useLocation } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { setCurrentUser } from '../../redux/user/user.action'
+import { UserState } from '../../redux/user/user.models'
+import { LoadingOutlined } from '@ant-design/icons'
 
 const NUMBER_OF_ITEMS_PER_PAGE = 3
 interface Post {
@@ -27,22 +27,40 @@ interface User {
     imageUrl: string
 }
 
-const Homepage = () => {
+interface Props {
+    user: UserState
+}
+
+const Homepage = (props: Props) => {
     const [posts, setPosts] = useState<Post[]>([])
-    const [user, setUser] = useState<{ username: string }>()
+    const [user, setUser] = useState<User>()
     const [hasMore, setHasMore] = useState(true)
     const [skip, setSkip] = useState(0)
     const path = useLocation()
     const [recommended, setRecommended] = useState<User[]>([])
 
-    const propTypes = {
-        articles: PropTypes.object.isRequired,
-        pagination: PropTypes.object.isRequired,
-    }
-
     useEffect(() => {
         document.title = 'Instagram'
+        console.log(props.user.currentUser)
+        if (props.user.currentUser) {
+            loadUserInfo()
+            loadRecommended()
+        }
+    }, [1])
 
+    const loadUserInfo = () => {
+        axios
+            .get(`${apiUrl}user/${props.user.currentUser?.userId}`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                },
+            })
+            .then((res) => {
+                setUser(res.data)
+            })
+    }
+
+    const loadRecommended = () => {
         axios
             .get(`${apiUrl}user?take=3`, {
                 headers: {
@@ -50,31 +68,35 @@ const Homepage = () => {
                 },
             })
             .then((res) => {
-                console.log(res.data.response.items)
                 setRecommended(res.data.response.items)
-            })
-    }, [1])
-
-    const loadItems = (numberOfItems: any) => {
-        // make loading bar appear here before we start fetching requests
-        axios
-            .get(`${apiUrl}p?take=3&skip=${skip}`, {
-                headers: {
-                    Authorization: `Bearer ${localStorage.getItem('token')}`,
-                },
-            })
-            .then((res) => {
-                setPosts([...posts, ...res.data.response.items])
-                const username = 'insert username here'
-                setUser({ username })
-                setSkip(skip + NUMBER_OF_ITEMS_PER_PAGE)
-                // make loading bar disappear here once we load all the shit ah
             })
     }
 
-    //<Comment content={props.commentContent} avatar={props.image} author={props.user} />
+    const loadItems = (numberOfItems: any) => {
+        // make loading bar appear here before we start fetching requests
 
-    let items: any[] = []
+        setTimeout(() => {
+            axios
+                .get(`${apiUrl}p?take=3&skip=${skip}`, {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('token')}`,
+                    },
+                })
+                .then((res) => {
+                    setPosts([...posts, ...res.data.response.items])
+                    setSkip(skip + NUMBER_OF_ITEMS_PER_PAGE)
+                    // make loading bar disappear here once we load all the shit ah
+                })
+        }, 1000)
+    }
+
+    const loader = () => {
+        return (
+            <div className="loader">
+                <LoadingOutlined />
+            </div>
+        )
+    }
 
     return (
         <div className="HomePageComponent">
@@ -82,7 +104,7 @@ const Homepage = () => {
                 <div className="storySection">
                     <Stories />
                 </div>
-                {posts.length && user
+                {posts.length
                     ? posts.map((item, i) => (
                           <div key={i} className="feedSection">
                               <Feed
@@ -95,21 +117,25 @@ const Homepage = () => {
                           </div>
                       ))
                     : null}
-                <InfiniteScroll
-                    pageStart={0}
-                    loadMore={loadItems}
-                    hasMore={true || false}
-                    loader={<div className="loader">Loading ...</div>}
-                >
-                    {items}
-                </InfiniteScroll>
+                <InfiniteScroll pageStart={0} loadMore={loadItems} hasMore={hasMore} loader={loader()} />
             </div>
-            <div className="HomePageSide">
-                    <User username={'ur'} fullName={'data'} profilePictureUrl={'here'} mainUser="true" />
+            {user ? (
+                <div className="HomePageSide">
+                    <User
+                        username={user.username}
+                        fullName={user.fullName}
+                        profilePictureUrl={user.imageUrl}
+                        mainUser="true"
+                    />
                     <Suggestion fetchedUsers={recommended} />
-            </div>
+                </div>
+            ) : null}
         </div>
     )
 }
 
-export default Homepage
+const mapStateToProps = (state: any) => ({
+    user: state.user,
+})
+
+export default connect(mapStateToProps)(Homepage)
